e0e196470b5ba615bb177d804facd2d3567f49c6
ReVaN <revannn5480@gmail.com>
2020-07-23 09:58:12 +0200

    Basic doctrine actions.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setName('Fiona');
+        $em->persist($user);
+        $em->flush();
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => $users
+        ]);
+    }
+}
